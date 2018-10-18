21caac17eba830c42b065707077b3fe203aa6278
Luiz Coke Neto <luiz.coke.neto@avanade.com>
2018-10-17 23:18:12 -0300

    feat(service): setup inicial api service

---
+++ service/service/ControleEstoqueContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using service.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace service
+{
+    public class ControleEstoqueContext : DbContext
+    {
+        public ControleEstoqueContext(DbContextOptions<ControleEstoqueContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(@"Server=.\;Database=EFCoreControleEstoque;Trusted_Connection=True;MultipleActiveResultSets=true");
+        //}
+    }
+}
---
+++ service/service/Model/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace service.Model
+{
+    public class Produto
+    {
+        [Key]
+        public int IdProduto { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public int Quantidade { get; set; }
+        public string Categoria { get; set; }
+        public string CodigoBarras { get; set; }
+    }
+}
